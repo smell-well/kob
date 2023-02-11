8922d69ce9da18b6a4d32c9e7e07376a87e1a18b
smell-well <625685664@qq.com>
2023-02-11 12:47:16 +0800

    导航栏-2

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,35 +1,16 @@
 <template>
-  <div>bot昵称: {{ bot_name }}</div>
-  <div>bot战力: {{ bot_rating }}</div>
+  <NavBar />
   <router-view/>
 </template>
 
 <script>
-
-import $ from 'jquery';
-import { ref } from 'vue'
-
+import NavBar from '@/components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default {
-  name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url: "http://localhost:3000/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        console.log(resp);
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating
-    }
+  components: {
+    NavBar,
   }
 }
 
