08c9bb6693f55d388f744c60c7283f3976d15af0
mengke yang <mengke.yang@outlook.com>
2023-02-15 16:53:31 +0800

    用户账号后端完成

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,11 +7,58 @@
 import NavBar from '@/components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-
+import $ from 'jquery'
 
 export default {
   components: {
     NavBar,
+  },
+
+  setup() {
+    $.ajax({
+      url: "http://localhost:3000/user/account/token/",
+      type: "post",
+      data: {
+        username: 'u2',
+        password: 'p2',
+      },
+      success(resp) {
+        console.log(resp)
+      },
+      error(resp) {
+        console.log(resp)
+      }
+    });
+
+    $.ajax({
+      url: "http://localhost:3000/user/account/info/",
+      type: "get",
+      headers: {
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlY2I0YjI2NjI3MTQ0YTFkYjQyNmE1YTRhNmViOTg2ZiIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY3NjQ0NjA1NiwiZXhwIjoxNjc3NjU1NjU2fQ.-me8nee4_mWR_j5-Rj9OFSM2b9JJuCuKvkG_ueplpUo",
+      },
+      success(resp) {
+        console.log(resp)
+      },
+      error(resp) {
+        console.log(resp)
+      }
+    });
+
+    $.ajax({
+      url: "http://localhost:3000/user/account/register/",
+      type: "post",
+      data: {
+        username: "yxc",
+        password: "123",
+        confirmedPasswrod: "123",
+      },
+      success(resp) {
+        console.log(resp)
+      },
+      error(resp) {
+        console.log(resp)
+      }
+    })
   }
 }
 
