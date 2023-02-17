f39f16e230bf882ff5d8d5a646f38bcf0afb7887
mengke yang <mengke.yang@outlook.com>
2023-02-17 14:39:24 +0800

    个人页面后端完成

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -5,10 +5,87 @@
 
 <script>
 import ContentField from '@/components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex'
 
 export default {
     components: {
         ContentField,
+    },
+    setup() {
+        const store = useStore();
+
+        // $.ajax({
+        //     url: "http://localhost:3000/user/bot/add/",
+        //     type: "POST",
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     data: {
+        //         title: "bot名称",
+        //         description: "bot描述",
+        //         content: "bot代码",
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: "http://localhost:3000/user/bot/remove/",
+        //     type: "POST",
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     data: {
+        //         bot_id: "1",
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: "http://localhost:3000/user/bot/update/",
+        //     type: "POST",
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     data: {
+        //         bot_id: "2",
+        //         title: "bot名称new",
+        //         description: "bot描述new",
+        //         content: "bot代码new",
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        $.ajax({
+            url: "http://localhost:3000/user/bot/getlist/",
+            type: "get",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            data: {
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        })
     }
 }
 </script>
