a140a6205cca0dfa110cdc7f998d95aebe7a468f
mengke yang <mengke.yang@outlook.com>
2023-02-21 22:57:59 +0800

    bot执行-1

---
+++ backendcloud/botrunningsystem/src/main/java/com/kob/botrunningsystem/BotRunningSystem.java
@@ -0,0 +1,11 @@
+package com.kob.botrunningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotRunningSystem {
+    public static void main(String[] args) {
+        SpringApplication.run(BotRunningSystem.class, args);
+    }
+}
